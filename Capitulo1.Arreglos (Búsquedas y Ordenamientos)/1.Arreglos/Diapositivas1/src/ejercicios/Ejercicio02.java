75e94dbba6fd43c7e4856260bb870b9b4e419c48
DayanaK11 <dayanacayol@gmail.com>
2024-05-02 13:25:42 -0500

    resolucion de la clase Ejercicio02 de Diapositivas1/ejercicios

---
+++ Capitulo1.Arreglos (Búsquedas y Ordenamientos)/1.Arreglos/Diapositivas1/src/ejercicios/Ejercicio02.java
@@ -0,0 +1,45 @@
+/* CONTENIDO
+ * Nombre: Dayana Cayo
+ * Fecha: 02/05/2024
+ * tiempo: 10min 49 seg
+ * 
+ * Hacer un programa que cree un arreglo con 
+ * capacidad para 100 números de celulares (que 
+ * sólo acepte números de 9 dígitos), que permita 
+ * ingresar los números requeridos hasta que el 
+ * usuario ingrese una “q” y finalmente que 
+ * imprima sólo los números ingresados. Confiar 
+ * que el usuario siempre ingresa dígitos. 
+ * 
+ * */
+
+package ejercicios;
+import java.util.*;
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner (System.in);
+		int tamaño=100;	
+		int contador=0;
+		String numero;
+		String contactos []=new String[tamaño];
+		
+		System.out.println("Ingresar numero: ");
+		numero=input.next();
+		
+		while(!(numero.equalsIgnoreCase("q"))&&(contactos.length==100)) {
+			if(numero.length()==9) {
+				contactos[contador]=numero;
+				contador++;
+			}
+			else
+				System.out.println("Ingrese un numero de 9 digitos");
+			System.out.println("Ingresar numero: ");
+			numero=input.next();
+		}
+		//imprimit
+		for(int i=0;i<contador;i++) {
+			System.out.println("Numero "+(i+1)+" : "+contactos[i]);
+		}
+	}
+}
