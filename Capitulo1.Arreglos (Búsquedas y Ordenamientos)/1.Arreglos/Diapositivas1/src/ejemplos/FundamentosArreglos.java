5ca1b7936e8c1552f5e7084413c4e74465ed5a29
DayanaK11 <dayanacayol@gmail.com>
2024-05-02 12:43:00 -0500

    Desarrollo de cla clase FundamentosArreglo

---
+++ Capitulo1.Arreglos (Búsquedas y Ordenamientos)/1.Arreglos/Diapositivas1/src/ejemplos/FundamentosArreglos.java
@@ -0,0 +1,21 @@
+package ejemplos;
+
+public class FundamentosArreglos {
+
+	public static void main(String[] args) {
+		//Declaracion de arreglos
+		//<tipoElemento> <nombreArreglo> [];
+		int  notasFP [];
+		
+		//Creación o dimencionamiento de Arreglos
+		//<nombreArreglo> = new <tipoElemto>[<tamaño>];
+		notasFP = new int [3];
+		
+		//combinacion de la declaracion y creacion
+		//<tipoElementos> <nombreArreglo>[] = new <tipoElementos>[<tamaño>];
+		String nombres []=new String [5];
+		
+		
+	}
+
+}
