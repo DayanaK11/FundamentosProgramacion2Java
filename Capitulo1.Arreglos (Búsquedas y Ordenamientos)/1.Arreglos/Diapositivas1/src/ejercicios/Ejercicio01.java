2155eb5e787224923075e5ba1585bb8ab4c98a98
DayanaK11 <dayanacayol@gmail.com>
2024-05-02 13:11:44 -0500

    resulucion de la clase Ejercicio01 de Diapositivas1/ejercicios

---
+++ Capitulo1.Arreglos (Búsquedas y Ordenamientos)/1.Arreglos/Diapositivas1/src/ejercicios/Ejercicio01.java
@@ -0,0 +1,41 @@
+/* CONTENIDO
+ * Nombre: Dayana Cayo
+ * Fecha: 02/05/2024
+ * Hacer un programa que cree un arreglo para n 
+ * notas, luego que permita ingresarlas y 
+ * finalmente que las imprima 
+*/
+package ejercicios;
+import java.util.*;
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner (System.in);
+		//declarando la variable n para el tamaño
+		int n;
+		//declarando arreglo notas
+		int notas[];
+		
+		//ingresando por teclado el tamaño
+		System.out.println("ingres cantidad de notas: ");
+		n=input.nextInt();
+		
+		//creando arreglo notas
+		notas=new int[n];
+		
+		for(int i=0;i<notas.length;i++) {
+			System.out.println("Ingresar nota "+(i+1)+" :");
+			notas[i]=input.nextInt();
+		}
+		
+		for(int i=0;i<notas.length;i++) {
+			System.out.println("Nota "+(i+1)+" : "+notas[i]);
+		}
+
+	}
+
+}
+/*puntos de mejora:
+ * no hay validacion de la notas en rango , en si son numeros
+*/
+ 
